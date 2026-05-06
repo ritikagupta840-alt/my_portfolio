5f1b26f666ce5be3e3ad756e151c78805e2c6f0d
ritika <ritikagupta840@gmail.com>
2026-05-07 00:17:49 +0530

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,11 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
+    allowedHosts: ["my-portfolio-bl4o.onrender.com"], 
     hmr: {
       overlay: false,
     },
